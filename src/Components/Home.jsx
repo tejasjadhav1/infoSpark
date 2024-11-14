156ac3a4dc2b70d7b8b4fceb936f4414c665c1b5
tejasjadhav1 <tejasjadhavofficial7@gmail.com>
2024-11-14 23:46:58 +0530

    styles

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import './Common.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Home() {
+  // State to track hover status
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
     <div className="dflex">
       <div className="left">
@@ -17,19 +20,39 @@ function Home() {
         <button className="knowmore">
           Know More <FontAwesomeIcon className='arrowRght' icon={faArrowRight} />
         </button>
-        <div className="footprints">
-          🌐
-          <p>Mumbai</p>
-          <p>Jalgaon</p>
+
+        <div className="dflex" style={{ justifyContent: 'space-between' }}>
+          <div className="footprints">
+            🌐
+            <p>Mumbai</p>
+            <p>Jalgaon</p>
+          </div>
+          <div 
+            className="chat" 
+         
+            style={{ position: 'relative' }} // Ensure the hover element is positioned correctly
+          >
+            <img 
+               onMouseEnter={() => setIsHovered(true)} 
+               onMouseLeave={() => setIsHovered(false)} 
+              className='chatimg' 
+              style={{ width: '40px', marginRight: '80px', cursor: 'pointer' }} 
+              src={`${process.env.PUBLIC_URL}/Images/chat.png`} 
+              alt="Chat Icon" 
+            />
+            <div className={`chatHover ${isHovered ? 'visible' : ''}`}>
+              Chat With Us
+            </div>
+          </div>
         </div>
       </div>
       <div className="right">
         {/* Additional content can go here */}
         <img 
-        src={`${process.env.PUBLIC_URL}/Images/network.jpg`} 
-        alt="Network" 
-        style={{ width: '100%', height: 'auto' }} // Optional: Adjust size as needed
-      />
+          src={`${process.env.PUBLIC_URL}/Images/network.jpg`} 
+          alt="Network" 
+          style={{ width: '100%', height: '100%' }} // Optional: Adjust size as needed
+        />
       </div>
     </div>
   );
